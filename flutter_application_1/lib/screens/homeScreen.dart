853894a49b6f78c7c987b166975cc6f3a3e95213
Fernando Guerrero <96fernando.guerrero@gmail.com>
2022-04-27 16:44:58 -0500

    Text Style
    Managed as constant to pass it to other widgets to avoid code duplicity

--- flutter_application_1/lib/screens/homeScreen.dart
+++ flutter_application_1/lib/screens/homeScreen.dart
@@ -8,11 +8,33 @@ class HomeScreen extends StatelessWidget {
   @override
   //Build Context: todo el arbol de widgets, mantiene toda la referencia hasta este punto
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.indigo,
-      body: Center(
-        child: Text('Home_Screen'),
-      ),
-    );
+    //tamaño de texto como constante:
+    const fontSize30 = TextStyle(fontSize: 30);
+
+    return Scaffold(
+        //backgroundColor: Colors.indigo,
+        //appbar no es un widget que tenga constante.
+        appBar: AppBar(
+          title: const Text('Home'),
+          elevation: 0.0,
+        ),
+        body: Center(
+          child: Column(
+            //de arriba a abajo
+            mainAxisAlignment: MainAxisAlignment.center,
+            //de izquireda a derecha
+            //crossAxisAlignment: CrossAxisAlignment.center,
+            children: const <Widget>[
+              Text(
+                'Numero de Taps',
+                style: fontSize30,
+              ),
+              Text(
+                "0",
+                style: fontSize30,
+              ),
+            ],
+          ),
+        ));
   }
 }
