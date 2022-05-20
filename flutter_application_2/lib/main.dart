29a2d34dcac2fcc4df68eaaebe6ca72fdb24adb8
Fernando Guerrero <96fernando.guerrero@gmail.com>
2022-05-20 16:54:35 -0500

    Before optimizing with models file and folder

--- flutter_application_2/lib/main.dart
+++ flutter_application_2/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_2/routes/app_routes.dart';
-import 'package:flutter_application_2/screens/screens.dart';
 
 void main() => runApp(MyApp());
 
